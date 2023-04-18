538f1f99071d2548e331038f49ad0b722bb5c753
Kate Goldenring <kate.goldenring@fermyon.com>
2023-04-18 23:31:09 +0200

    Add typescript example and remove no longer necessary sections
    Signed-off-by: Kate Goldenring <kate.goldenring@fermyon.com>

--- apps/04/hello-rust/src/lib.rs
+++ apps/04/hello-rust/src/lib.rs
@@ -4,12 +4,12 @@ use spin_sdk::{
     http_component,
 };
 
-/// A simple Spin HTTP component.
+/// The entrypoint to our Spin component.
 #[http_component]
 fn handle_hello_rust(req: Request) -> Result<Response> {
     println!("{:?}", req.headers());
     Ok(http::Response::builder()
         .status(200)
         .header("foo", "bar")
-        .body(Some("Hello, Fermyon".into()))?)
+        .body(Some("Hello, WebAssembly!".into()))?)
 }
